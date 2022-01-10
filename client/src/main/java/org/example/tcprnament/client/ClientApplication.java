9770eed7f9297f7bd66a11298e8673d7e101460a
Aleksander Szamałek <aleksander.szamalek@gmail.com>
2022-01-10 15:19:34 +0100

    Initial project structure, establishing submodules

---
+++ client/src/main/java/org/example/tcprnament/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package org.example.tcprnament.client;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        log.info("--------------------Starting client application-------------------------");
+        log.info("test");
+        log.error("error");
+    }
+}
---
+++ shared/src/main/java/org/example/tcprnament/shared/TestClass.java
@@ -0,0 +1,10 @@
+package org.example.tcprnament.shared;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TestClass {
+    private String name;
+}
