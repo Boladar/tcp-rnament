fe20b1922315613e8f6b3caa9b8328ecf9da509f
xVulpeSx <jeremi.lisek@gmail.com>
2022-01-14 23:51:10 +0100

    all serverCommands handled

--- client/src/main/java/org/example/tcprnament/client/TournamentProtocolApplication.java
+++ client/src/main/java/org/example/tcprnament/client/TournamentProtocolApplication.java
@@ -26,46 +26,58 @@ public class TournamentProtocolApplication extends ServerCommandParser {
 
     @Override
     protected void onGameCreated(GameCreatedCommand command) {
-        System.out.println("!GÓWNO OŻYŁO!");
+        log.info("Game: " + command.getName() + " created.");
+        //wrzucic do pokoju ktory sie stworzylo
     }
 
     @Override
     protected void onGamesList(GamesListCommand command) {
-
+        log.info("Received " + command.getGames().size() + " games list.");
+        gui.gameListReceived(command.getGames());
     }
 
     @Override
     protected void onGameJoined(GameJoinedCommand command) {
-
+        log.info("Player joined " + command.getName() + " game.");
+        gui.gameJoined();
     }
 
     @Override
     protected void onGameStarted(ServerCommand command) {
-
+        log.info("Game started.");
     }
 
     @Override
     protected void onCommandRejected(CommandRejected command) {
-        log.info("Brawo cos zjebales! " + command.getReason());
+        log.info("Last command rejected! Reason: " + command.getReason());
+        gui.rejectedInfoMessage(command.getReason());
     }
 
     @Override
     protected void onPlayerJoined(PlayerJoinedCommand command) {
-
+        log.info("Player " + command.getPlayerName() + " joined lobby.");
+        gui.addPlayer(command.getPlayerName());
     }
 
     @Override
     protected void onPlayerLeft(PlayerLeftCommand command) {
-
+        log.info("Player " + command.getPlayerName() + " left lobby.");
+        gui.removePlayer(command.getPlayerName());
     }
 
     @Override
     protected void onGameScoreUpdate(GameScoreUpdateCommand command) {
-
+        log.info("Received new gameScore: " + command.getScoreboard());
+        if(command.isFinished()){
+            log.info("Game ended");
+            gui.gameFinished();
+        }
+        gui.updateScoreBoard(command.getScoreboard());
     }
 
     @Override
     protected void onGameQuestion(GameQuestionCommand command) {
-
+        log.info("New question: " + command.getQuestionNumber() + ". " + command.getText());
+        gui.newQuestion(command.getQuestionNumber(), command.getText());
     }
 }
