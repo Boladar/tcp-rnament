1ccd9e7744df3270ae4769e3c38bb65f130cd028
xVulpeSx <jeremi.lisek@gmail.com>
2022-01-15 18:10:28 +0100

    added getGameList

--- client/src/main/java/org/example/tcprnament/client/Sender.java
+++ client/src/main/java/org/example/tcprnament/client/Sender.java
@@ -4,6 +4,8 @@ package org.example.tcprnament.client;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.example.tcprnament.shared.commands.client.ClientCommand;
+import org.example.tcprnament.shared.commands.client.ClientCommandType;
 import org.example.tcprnament.shared.commands.client.concrete.CreateGameCommand;
 import org.springframework.integration.ip.tcp.serializer.ByteArrayCrLfSerializer;
 
@@ -28,6 +30,12 @@ public class Sender {
     public void sendCreateGame(String name, String password) throws IOException {
         log.info("CreateGameCommand: Name: " + name + ", password: " + password);
         tournamentProtocolApplication.write(new CreateGameCommand(commandCounter, name, password), clientSocket.getOutputStream());
+        commandCounter++;
+    }
+
+    public void getGameList() throws IOException {
+        log.info("Sending gameList request");
+        tournamentProtocolApplication.write(new ClientCommand(commandCounter, ClientCommandType.SHOW_GAMES), clientSocket.getOutputStream());
         commandCounter++;
     }
 }
